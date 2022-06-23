17e98fa2909b2d75533e63c0eb93bee483b56e17
aierh1004 <penny.whe@gmail.com>
2022-06-23 16:23:17 +0800

    3 files modified, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
-export default {};
+export default {
+  components: TheHeader,
+};
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
