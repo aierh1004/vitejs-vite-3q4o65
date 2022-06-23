ad1e38f3940fad3e287108d0e4d3a83512d02955
aierh1004 <penny.whe@gmail.com>
2022-06-23 17:30:49 +0800

    4 files modified

--- src/pages/Coaches/CoachList.vue
+++ src/pages/Coaches/CoachList.vue
@@ -2,5 +2,13 @@
   <section>
     <h1>HELLO</h1>
   </section>
-  <section>WORLD</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register a Coach</router-link>
+    </div>
+    <ul>
+      List of Coaches
+    </ul>
+  </section>
 </template>
---
+++ src/pages/requests/RequestReceived.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>
+    <h1>HELLO</h1>
+  </section>
+  <section>Requests</section>
+</template>
