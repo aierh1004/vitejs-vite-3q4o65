c286d2e0f279ed67d33f4baf3e49257dd6b866c4
aierh1004 <penny.whe@gmail.com>
2022-06-27 09:37:31 +0800

    3 files modified, 3 added

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,73 @@
+<template>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="mode">
+    <slot> </slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    link: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    to: {
+      type: String,
+      required: false,
+      default: '/',
+    },
+  },
+};
+</script>
+
+<style scoped>
+button,
+a {
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: #0066cc;
+  border: 1px solid #0066cc;
+  color: white;
+  cursor: pointer;
+  border-radius: 30px;
+  margin-right: 0.5rem;
+  display: inline-block;
+}
+
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: #0066cc;
+  border-color: #0066cc;
+}
+
+.flat {
+  background-color: transparent;
+  color: #99ccff;
+  border: none;
+}
+
+.outline {
+  background-color: transparent;
+  border-color: #0066cc;
+  color: #0066cc;
+}
+
+.flat:hover,
+.flat:active,
+.outline:hover,
+.outline:active {
+  background-color: #99ccff;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import store from './store/index.js'
-import {router} from './utils/router.js';
+import store from './store/index.js';
+import { router } from './utils/router.js';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
 const app = createApp(App);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.use(router);
 app.use(store);
