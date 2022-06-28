15ca618a7ba4a33f0cfe913a2ebd72fcb2d189bc
aierh1004 <penny.whe@gmail.com>
2022-06-29 00:29:46 +0800

    3 files modified

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,7 +2,7 @@ export default {
   login() {},
   async signup(context, payload) {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB4GigzbGpaKNgcfMMHR1qILFvf1Y9we4g',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB4GigzbGpaKNgcfMMHR1qILFvf1Y9we4g',
       {
         method: 'POST',
         body: JSON.stringify({
@@ -16,7 +16,7 @@ export default {
     if (!response.ok) {
       console.log(responseData);
       const error = new Error(
-        responseData.message || 'Failed to authenticate.'
+        responseData.message || 'Failed to authenticate. Check your login data.'
       );
       throw error;
     }
