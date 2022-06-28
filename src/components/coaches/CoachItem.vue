c66e6d8400f8773d6113a9b90eb77d4ac140c9ef
aierh1004 <penny.whe@gmail.com>
2022-06-28 11:18:33 +0800

    1 file modified

--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -27,7 +27,7 @@ export default {
       return this.firstName + ' ' + this.lastName;
     },
     coachContactLink() {
-      return this.$route.path + '/' + this.id + '/conatact';
+      return this.$route.path + '/' + this.id + '/contact';
     },
     coachDetailsLink() {
       return this.$route.path + '/' + this.id;
